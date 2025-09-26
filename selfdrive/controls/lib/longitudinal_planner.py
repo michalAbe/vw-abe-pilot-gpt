06dfd9175cae61042a5d5f95c1b63e23fdaefb78
michalAbe <abebeats@gmail.com>
2025-09-26 15:41:28 +0200

    Update longitudinal_planner.py

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -16,9 +16,8 @@ from openpilot.selfdrive.car.cruise import V_CRUISE_MAX, V_CRUISE_UNSET
 from openpilot.common.swaglog import cloudlog
 from selfdrive.controls.lib.traffic_lights import TrafficLightController
 from openpilot.common.params import Params
-from openpilot.common.params import Params
-from openpilot.selfdrive.controls.lib.traffic_lights import TrafficLightController
-from openpilot.sunnypilot.selfdrive.controls.lib.longitudinal_planner import LongitudinalPlannerSP
+
+
 
 LON_MPC_STEP = 0.2  # first step is 0.2s
 A_CRUISE_MAX_VALS = [1.6, 1.2, 0.8, 0.6]
