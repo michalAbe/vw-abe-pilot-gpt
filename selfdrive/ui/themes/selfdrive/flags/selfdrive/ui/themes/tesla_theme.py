05092b438775ae587c5c5836942446a7e997826b
michalAbe <abebeats@gmail.com>
2025-10-08 17:03:37 +0200

    Create tesla_theme.py
    Add Tesla-style theme (visual only)

---
+++ selfdrive/ui/themes/selfdrive/flags/selfdrive/ui/themes/tesla_theme.py
@@ -0,0 +1,9 @@
+# Simple Tesla-like theme settings
+
+theme = {
+    "background_color": "#000000",
+    "primary_color": "#00ff00",
+    "accent_color": "#ff0000",
+    "font": "Roboto",
+    "logo_path": "selfdrive/ui/assets/tesla_logo.png"
+}
