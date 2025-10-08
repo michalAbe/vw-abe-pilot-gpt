e7f6ef522f2949289660c464adb091ad5d0f1b74
michalAbe <abebeats@gmail.com>
2025-10-08 18:12:27 +0200

    Create tesla_skoda_startup_v2.py
    - Added a new widget `TeslaSkodaStartup` in selfdrive/ui/qt/widgets/
    - Displays Tesla-Skoda logo at startup
    - Logo is centered and scaled to 60% of screen width
    - Can optionally hide after a few seconds using QTimer
    - Added assets/images/tesla_skoda.png for the logo
    - No existing UI files were modified; widget is added safely

---
+++ selfdrive/ui/widgets/tesla_skoda_startup_v2.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QPixmap, QPainter
+
+class TeslaSkodaStartup(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.logo = QPixmap("selfdrive/ui/qt/assets/images/tesla_skoda.png")  # cesta k tvojmu logu
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        
+        width = self.width()
+        height = self.height()
+
+        # veľkosť loga (60% šírky obrazovky)
+        logo_width = int(width * 0.6)
+        logo_height = int(self.logo.height() * (logo_width / self.logo.width()))
+
+        # stred obrazovky
+        x = (width - logo_width) // 2
+        y = (height - logo_height) // 2
+
+        painter.drawPixmap(x, y, logo_width, logo_height, self.logo)
