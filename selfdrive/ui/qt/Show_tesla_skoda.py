1e5cf38dc689f968c156d7f004bd622eb97bc9db
michalAbe <abebeats@gmail.com>
2025-10-08 19:07:03 +0200

    Create Show_tesla_skoda.py
    Add standalone Tesla-Skoda startup logo widget

---
+++ selfdrive/ui/qt/Show_tesla_skoda.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPixmap, QPainter
+from PyQt5.QtCore import QTimer
+
+# Cesta k tvojmu logu
+LOGO_PATH = "selfdrive/ui/qt/assets/images/tesla_skoda.png"
+
+class TeslaSkodaStartup(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.logo = QPixmap(LOGO_PATH)
+        self.setWindowTitle("Tesla-Škoda Startup")
+        self.showFullScreen()  # celé okno
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        
+        width = self.width()
+        height = self.height()
+
+        logo_width = int(width * 0.6)
+        logo_height = int(self.logo.height() * (logo_width / self.logo.width()))
+
+        x = (width - logo_width) // 2
+        y = (height - logo_height) // 2
+
+        painter.drawPixmap(x, y, logo_width, logo_height, self.logo)
+
+# Spustenie aplikácie
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = TeslaSkodaStartup()
+    window.show()
+
+    # Skryje logo po 3 sekundách
+    QTimer.singleShot(3000, window.close)
+
+    sys.exit(app.exec_())
